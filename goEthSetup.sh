e85ff4f31522adbe8acf90fa5e90437fc71100de
walker088 <hanxu2121@gmail.com>
2018-01-22 19:32:26 +0000

    goeth setup

---
+++ goEthSetup.sh
@@ -0,0 +1,10 @@
+curl -O https://storage.googleapis.com/golang/go1.7.3.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.7.3.linux-amd64.tar.gz
+rm go1.7.3.linux-amd64.tar.gz
+mkdir -p ~/go; echo "export GOPATH=$HOME/go" >> ~/.bashrc
+echo "export PATH=$PATH:$HOME/go/bin:/usr/local/go/bin" >> ~/.bashrc
+source ~/.bashrc
+echo "----------------------------golang installed!----------------------------"
+
+sudo apt-get install -y build-essential golang
+echo "---------------------------- env settle done ----------------------------"
