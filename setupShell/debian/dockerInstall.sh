4fdc9e765f4080ae846f32f7c3cfd5224a245001
walker088 <hanxu2121@gmail.com>
2019-09-30 04:51:12 -0400

    docker, pg setup shells

---
+++ setupShell/debian/dockerInstall.sh
@@ -0,0 +1,20 @@
+sudo true
+
+sudo apt-get remove docker docker-engine docker.io
+sudo apt-get update
+sudo apt-get install \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     gnupg2 \
+     software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce
+
+sudo usermod -a -G docker $USER
---
+++ setupShell/debian/pginstall.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ buster-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+sudo apt-get update
+sudo apt-get install postgresql postgresql-contrib libpq-dev
+
