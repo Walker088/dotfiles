e15a2f8003bf0257e1cce65313a2dcb6d9a28ae4
walker088 <walker088@walker088.tw>
2024-04-05 16:33:39 -0400

    chore: Modified cuda, added fish and python install bash

--- shell/cuda.sh
+++ shell/cuda.sh
@@ -1,6 +1,8 @@
 distro="ubuntu2204"
 arch="x86_64"
 
+sudo ubuntu-drivers install
+
 wget https://developer.download.nvidia.com/compute/cuda/repos/$distro/$arch/cuda-keyring_1.1-1_all.deb
 sudo dpkg -i cuda-keyring_1.1-1_all.deb
 
---
+++ shell/fish.sh
@@ -0,0 +1,7 @@
+sudo apt-add-repository ppa:fish-shell/release-3
+sudo apt update
+sudo apt install fish
+
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+fisher install jorgebucaran/nvm.fish
+fisher install reitzig/sdkman-for-fish@v2.1.0
